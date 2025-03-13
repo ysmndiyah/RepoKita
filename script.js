4d8186f2e91a4bb97e80295b3af2ef7fe6730da7
ysmndiyah <sakdiyahh845@gmail.com>
2025-03-13 09:11:59 +0700

    rubah animate name

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 document.getElementById('menu-toggle').addEventListener('click', function () {
     document.getElementById('navbar').classList.toggle('active');
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const text = "Al Yasmin Assa'diyah"; 
+    const typingElement = document.querySelector(".typing-effect");
+    let index = 0;
+
+    function typeText() {
+        if (index < text.length) {
+            typingElement.textContent += text.charAt(index);
+            index++;
+            setTimeout(typeText, 100); 
+        }
+    }
+
+    typeText();
+
+   
+    document.querySelector(".fade-in").style.animationDelay = "2s";
+});
