e2f8a1ba6094e18e2a2234bd36dec0339384a05e
ysmndiyah <sakdiyahh845@gmail.com>
2025-03-19 22:54:25 +0700

    change

--- about.js
+++ about.js
@@ -1,21 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const text = "Perkenalkan, nama saya 'Al Yasmin Assa'diyah', biasa dipanggil Mimin.";
-    const typingElement = document.querySelector(".typing-effect");
-    let index = 0;
-    
-    function typeText() {
-        if (index < text.length) {
-            typingElement.textContent += text.charAt(index);
-            index++;
-            setTimeout(typeText, 100);
-        }
-    }
-    typeText();
-    
-    const menuToggle = document.getElementById("menu-toggle");
-    const navbar = document.getElementById("navbar");
-    
-    menuToggle.addEventListener("click", function () {
-        navbar.classList.toggle("active");
-    });
-});
+
+        document.addEventListener("DOMContentLoaded", function () {
+            const text = "Perkenalkan, nama saya 'Al Yasmin Assa'diyah', biasa dipanggil Mimin.";
+            const typingElement = document.querySelector(".typing-effect");
+            let index = 0;
+            
+            function typeText() {
+                if (index < text.length) {
+                    typingElement.textContent += text.charAt(index);
+                    index++;
+                    setTimeout(typeText, 100);
+                }
+            }
+            typeText();
+            
+            const menuToggle = document.getElementById("menu-toggle");
+            const navbar = document.getElementById("navbar");
+            
+            menuToggle.addEventListener("click", function () {
+                navbar.classList.toggle("active");
+            });
+        });
+   
