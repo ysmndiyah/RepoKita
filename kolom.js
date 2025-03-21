4d392bc73947dd21cee6539afa98d0ce514542ad
ysmndiyah <sakdiyahh845@gmail.com>
2025-03-21 22:21:53 +0700

    kolom

---
+++ kolom.js
@@ -0,0 +1,19 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    emailjs.sendForm("service_p8is3he", "template_5yw2so8", this)
+        .then(function(response) {
+            alert("Pesan berhasil dikirim! ✅");
+            console.log("SUCCESS!", response.status, response.text);
+        }, function(error) {
+            alert("Pesan gagal dikirim. ❌ Cek konfigurasi EmailJS.");
+            console.error("FAILED...", error);
+        });
+});
+        const formData = {
+            from_name: document.getElementById("name").value,
+            email_id: document.getElementById("email").value,
+            message: document.getElementById("message").value,
+        };
+
+        
