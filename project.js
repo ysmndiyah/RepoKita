5f5ac719bac96f205d6192cefd95ca4cd485e262
ysmndiyah <sakdiyahh845@gmail.com>
2025-03-27 15:15:04 +0700

    maw

--- project.js
+++ project.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Project Page Loaded!");
 });
-// Get the menu toggle button and the navbar
+
 const menuToggle = document.getElementById('menu-toggle');
 const navbar = document.getElementById('navbar');
 
-// Add a click event listener to the menu toggle
+
 menuToggle.addEventListener('click', () => {
-    // Toggle the 'active' class on the navbar
+   
     navbar.classList.toggle('active');
 });
